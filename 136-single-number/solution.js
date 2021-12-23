f2d29cca60bcf2a692cc06ca34b9779788252122
Volodymyr Sirenko <volodymyr.sirenko@gmail.com>
2021-12-23 20:42:31 +0200

    add solution to "136. Single Number"

---
+++ 136-single-number/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNumber(nums) {
+  let res = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    res = res ^ nums[i];
+  }
+
+  return res;
+}
