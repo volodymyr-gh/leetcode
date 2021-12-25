2c9175c0365d24c9d01171a468a05b56fdd0d4bc
Volodymyr Sirenko <volodymyr.sirenko@gmail.com>
2021-12-25 13:27:41 +0200

    add solution to "20. Valid Parentheses"

---
+++ 20-valid-parentheses/solution.js
@@ -0,0 +1,43 @@
+const bracketsMap = {
+  ')': '(',
+  '}': '{',
+  ']': '['
+};
+
+function isOpenBracket(ch) {
+  return ch === '(' || ch === '{' || ch === '[';
+}
+
+function isCloseBracket(ch) {
+  return ch === ')' || ch === '}' || ch === ']';
+}
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+  const open = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = s[i];
+
+    if (isOpenBracket(curr)) {
+      open.push(curr);
+      continue;
+    }
+
+    if (!open.length) {
+      return false;
+    }
+
+    const lastOpen = open[open.length - 1];
+    if (bracketsMap[curr] !== lastOpen) {
+      return false;
+    }
+
+    open.pop();
+  }
+
+  return !open.length;
+}
