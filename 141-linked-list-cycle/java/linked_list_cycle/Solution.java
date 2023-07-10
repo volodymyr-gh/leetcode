846f67470c4d770ebc8149527995a443939b040f
volodymyr-gh <volodymyr.sirenko@gmail.com>
2023-07-10 21:04:37 +0300

    update java solution for "141. Linked List Cycle"

--- 141-linked-list-cycle/java/linked_list_cycle/Solution.java
+++ 141-linked-list-cycle/java/linked_list_cycle/Solution.java
@@ -32,18 +32,15 @@ public class Solution {
     }
 
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
+
         while (fast != null && fast.next != null) {
-            if (slow.equals(fast)) {
-                return true;
-            }
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
         }
 
         return false;
