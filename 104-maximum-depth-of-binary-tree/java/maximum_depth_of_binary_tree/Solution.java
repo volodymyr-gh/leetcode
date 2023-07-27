a6b3cc7f8150f6650db54a60fec4b217309e6410
volodymyr-gh <volodymyr.sirenko@gmail.com>
2023-07-27 11:32:29 +0300

    add java solution for "104. Maximum Depth of Binary Tree"

---
+++ 104-maximum-depth-of-binary-tree/java/maximum_depth_of_binary_tree/Solution.java
@@ -0,0 +1,38 @@
+package maximum_depth_of_binary_tree;
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(9);
+        root1.right = new TreeNode(20);
+        root1.right.left = new TreeNode(15);
+        root1.right.right = new TreeNode(7);
+        int res1 = new Solution().maxDepth(root1);
+        assert res1 == 3;
+
+        TreeNode root2 = new TreeNode(1);
+        root2.right = new TreeNode(2);
+        int res2 = new Solution().maxDepth(root2);
+        assert res2== 2;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lDepth = maxDepth(root.left) + 1;
+        int rDepth = maxDepth(root.right) + 1;
+
+        return Math.max(lDepth, rDepth);
+    }
+}
